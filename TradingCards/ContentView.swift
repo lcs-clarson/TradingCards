cc32c3b789328fd5f184936a7318c0359b053e69
Charlie Larson <clarson@lcs.on.ca>
2024-01-18 14:32:57 -0500

    Made very small progress but I have some fixing to do.

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -7,15 +7,33 @@
 
 import SwiftUI
 
+struct HockeyCard: View {
+    var playerName: String
+    var imageName: String
+
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 200) // Set the height of the image, adjusting the width accordingly
+
+            Text(playerName)
+                .font(.headline)
+                .padding()
+        }
+        .border(Color.gray, width: 1)
+        .padding()
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HockeyCard(playerName: "Alex Ovechkin", imageName: "Image")
+            HockeyCard(playerName: "Sidney Crosby", imageName: "Image 2")
+            HockeyCard(playerName: "Connor Mcdavid", imageName: "Image 1")
         }
-        .padding()
     }
 }
 
