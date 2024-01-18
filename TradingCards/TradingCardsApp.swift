d9bec2d966740453c97128d449e60303e3e893d9
Charlie Larson <clarson@lcs.on.ca>
2024-01-18 14:08:09 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Charlie Larson on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
